7622305861427dfe1fd11711743fcd616a9a52fc
shubham <shubham@metarvrse.com>
2017-05-26 18:51:35 +0530

    Project files along with script for customising EditorGUILayout.FloatField()

---
+++ Assets/MeshGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[RequireComponent(typeof(MeshFilter))] [ExecuteInEditMode]
+public class MeshGenerator : MonoBehaviour {
+
+	public Material defaultMat;
+	[HideInInspector]
+	public bool isPlane;
+	[HideInInspector]
+	public float heightOfPlane;
+	[HideInInspector]
+	public float widthOfPlane;
+
+	MeshFilter meshFilter;
+	MeshRenderer meshRend;
+
+	List<Vector3> vertices = new List<Vector3>();
+	List<Vector3> trianlges = new List<Vector3>();
+	List<Vector3> normals = new List<Vector3>();
+
+	void Start()
+	{
+		meshFilter = GetComponent<MeshFilter> ();
+		meshRend = GetComponent<MeshRenderer> ();
+
+		Mesh mesh = new Mesh ();
+
+
+		meshFilter.mesh = mesh;
+		print ("running");
+	}
+
+	void SetPlane()
+	{
+		
+	}
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(MeshGenerator))]
+public class MeshGenerator_Editor: Editor{
+
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector ();
+		MeshGenerator script = (MeshGenerator)target;
+		script.isPlane = EditorGUILayout.Toggle ("Is Plane", script.isPlane);
+
+		if (script.isPlane) {
+			script.heightOfPlane = EditorGUILayout.FloatField ("Height Of Plane", script.heightOfPlane, null);
+			script.widthOfPlane = EditorGUILayout.FloatField ("Width Of Plane", script.heightOfPlane, null);
+		}
+	}
+}
+#endif
