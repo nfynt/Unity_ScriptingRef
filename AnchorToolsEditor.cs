edd85e28b3594d7b4e0fc53cde93cd4b85ed39b1
Shubham Singh <shubham.nfynt@gmail.com>
2018-03-15 22:18:23 +0530

    Update AnchorToolsEditor.cs

--- AnchorToolsEditor.cs
+++ AnchorToolsEditor.cs
@@ -22,5 +22,24 @@ public class UIAnchors {
 			t.offsetMin = t.offsetMax = new Vector2(0, 0);
 		}
 	}
+	
+	[MenuItem("Nuspace/Build Settings/Android")]
+	static void SetAndroidSettings()
+	{
+		UnityEditor.PlayerSettings.companyName = "Imaginate";
+		UnityEditor.PlayerSettings.productName = "Nuspace";
+		UnityEditor.PlayerSettings.runInBackground = true;
+		UnityEditor.PlayerSettings.defaultInterfaceOrientation = UIOrientation.LandscapeLeft;
+		UnityEditor.PlayerSettings.SplashScreen.show = false;
+		UnityEditor.PlayerSettings.applicationIdentifier = "com.imaginate.nuspace";
+		UnityEditor.PlayerSettings.bundleVersion = "";
+		UnityEditor.PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel21;
+		UnityEditor.PlayerSettings.SetScriptingBackend (BuildTargetGroup.Android, ScriptingImplementation.Mono2x);
+		UnityEditor.PlayerSettings.SetApiCompatibilityLevel (BuildTargetGroup.Android, ApiCompatibilityLevel.NET_2_0);
+		UnityEditor.PlayerSettings.Android.targetDevice = AndroidTargetDevice.ARMv7;
+		UnityEditor.PlayerSettings.Android.preferredInstallLocation = AndroidPreferredInstallLocation.PreferExternal;
+		UnityEditor.PlayerSettings.Android.forceSDCardPermission = true;
+		UnityEditor.PlayerSettings.Android.androidTVCompatibility = false;
+	}
 
 }
