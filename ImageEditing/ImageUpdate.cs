ff3cd001d8b1633e937b29e11e026b60f85ffbfe
Shubham Singh <shubham.nfynt@gmail.com>
2018-03-30 19:28:44 +0530

    image editing

---
+++ ImageEditing/ImageUpdate.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageUpdate : MonoBehaviour {
+
+	public Texture2D tex;
+	public int brushSize = 15;
+	[Range(0,1)]
+	public float brushAlpha = 0f;
+
+	Color32[] texPix;
+	int width,height;
+
+
+	void Start()
+	{
+		//Initialising image components
+		GetComponent<RawImage> ().texture = tex;
+		width = tex.width;
+		height = tex.height;
+		texPix = tex.GetPixels32 ();
+		tex = new Texture2D (width, height, TextureFormat.ARGB32,false);
+		tex.SetPixels32 (texPix);
+	}
+
+	public void UpdateSizeAndAlpha()
+	{
+		
+	}
+
+	void Update()
+	{
+		//Update this to touch for mobile devices
+		if (Input.GetMouseButton (0)) {
+			Vector2 pixelPos = Input.mousePosition;
+
+			//Removing the touched pixels
+			for (int i = -brushSize; i < brushSize; i++)
+				for (int j = -brushSize; j < brushSize; j++) {
+					Color col = tex.GetPixel (i + (int)pixelPos.x, j + (int)pixelPos.y);
+					tex.SetPixel ((int)Mathf.Clamp (i + pixelPos.x, 0, width), (int)Mathf.Clamp (j + (int)pixelPos.y, 0, height), new Color (col.r, col.g, col.b, brushAlpha));
+				}
+			
+			UpdateImage ();
+		}
+	}
+
+	void UpdateImage()
+	{
+		//tex = new Texture2D (width, height, TextureFormat.ARGB32,false);
+		//tex.SetPixels32 (texPix);
+		//Applying the updates
+		tex.Apply ();
+		GetComponent<RawImage> ().texture = tex;
+	}
+}
---
+++ ImageEditing/README.md
@@ -0,0 +1,5 @@
+```
+Allows to user to superimpose mutliple image layers and selectively operate on one layer. As of now it contains only manipulating alpha channel of image with no layer selection option from UI.
+```
+
+![TRAFFIC DATA](https://github.com/nfynt/Unity_ScriptingRef/blob/master/ImageEditing/superimposing.PNG?raw=true)
