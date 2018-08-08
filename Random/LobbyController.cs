9fcf7f5b748a0b76c85c3ec564307775e494cc7e
Shubham Singh <shubham.nfynt@gmail.com>
2018-08-08 23:06:58 +0530

    Photon network manager and lobby script

---
+++ Random/LobbyController.cs
@@ -0,0 +1,229 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Shubham.Networking
+{
+    public class LobbyController : Singleton<LobbyController>
+    {
+        public GameObject homePanel;
+        [Space(10)]
+        [Header("Host Panel Component")]
+        public GameObject hostPanel;
+        public Button createBtn;
+        [Space(10)]
+        [Header("Join Panel Component")]
+        public GameObject joinPanel;
+        public Button joinBtn;
+        public GameObject roomListContainer;
+        [Space(10)]
+        [Header("Game Panel Component")]
+        public GameObject gamePanel;
+        public Button gameStartBtn;
+        public GameObject playerListContainer;
+        [HideInInspector]
+        public NetworkManager netManager;
+
+
+        private void Start()
+        {
+            SwitchToHomePanel();
+        }
+
+        private void OnDisable()
+        {
+            NetworkManager.RoomJoined -= ShowUsersList;
+            NetworkManager.NewPlayerJoined -= RefreshPlayersList;
+        }
+
+        public void CreateRoom(InputField roomName)
+        {
+            NetworkManager.RoomJoined += ShowUsersList;
+            netManager.StartOrJoinRoom(roomName.text, true);
+        }
+
+        public void JoinRoom(int index)
+        {
+            NetworkManager.RoomJoined += ShowUsersList;
+            netManager.StartOrJoinRoom(netManager.roomList[index], false);
+        }
+
+        public void ShowUsersList(bool success)
+        {
+            NetworkManager.RoomJoined -= ShowUsersList;
+            if (success)
+            {
+               SwitchToGamePanel();
+            }
+            else
+            {
+                ShowNotification("Failed to join room");
+            }
+        }
+
+        public void ShowNotification(string msg)
+        {
+            Debug.LogError(msg);
+        }
+
+        public void ToggleButtonInteractable(Button btn)
+        {
+            btn.interactable = !btn.interactable;
+        }
+
+        public void SwitchToHomePanel()
+        {
+            hostPanel.SetActive(false);
+            joinPanel.SetActive(false);
+            homePanel.SetActive(true);
+            gamePanel.SetActive(false);
+        }
+
+        public void SwitchToHostPanel()
+        {
+            hostPanel.SetActive(true);
+            joinPanel.SetActive(false);
+            homePanel.SetActive(false);
+
+        }
+
+        public void SwitchToJoinPanel()
+        {
+            hostPanel.SetActive(false);
+            joinPanel.SetActive(true);
+            homePanel.SetActive(false);
+            PopulateRoomList();
+        }
+        
+        public void StartGame()
+        {
+            NetworkManager.NewPlayerJoined -= RefreshPlayersList;
+            //PhotonNetwork.LoadLevel("scene name");
+        }
+
+        public void RefreshPlayersList(PhotonPlayer pp)
+        {
+            PopulateParticipantsList();
+        }
+
+        void SwitchToGamePanel()
+        {
+            hostPanel.SetActive(false);
+            joinPanel.SetActive(false);
+            gamePanel.SetActive(true);
+
+            gameStartBtn.onClick.RemoveAllListeners();
+            gameStartBtn.onClick.AddListener(StartGame);
+
+            if (netManager.IsHost)
+                gameStartBtn.interactable = true;
+            else
+                gameStartBtn.interactable = false;
+
+            PopulateParticipantsList();
+
+            NetworkManager.NewPlayerJoined += RefreshPlayersList;
+        }
+
+        void PopulateParticipantsList()
+        {
+            netManager.RefreshParticipantsList(netManager.RoomName);
+
+            int cnt = netManager.ParticipantsCount;
+
+            if (cnt == 0)
+            {
+                playerListContainer.transform.GetChild(0).GetChild(0).GetComponent<Button>().interactable = false;
+                playerListContainer.transform.GetChild(0).GetChild(1).GetComponent<Text>().text = "NA";
+                return;
+            }
+
+            for (int i = playerListContainer.transform.childCount - 1; i > 0; i--)
+                Destroy(playerListContainer.transform.GetChild(i));
+
+            for (int i = 0; i < cnt; i++)
+            {
+                if (i == 0)
+                {
+                    if (netManager.IsHost)
+                    {
+                        playerListContainer.transform.GetChild(0).GetChild(0).GetComponent<Button>().interactable = false;
+                        //playerListContainer.transform.GetChild(0).GetChild(0).GetComponent<Button>().onClick.RemoveAllListeners();
+                        //playerListContainer.transform.GetChild(0).GetChild(0).GetComponent<Button>().onClick.AddListener(delegate
+                        //{
+                        //    PhotonPlayer pp = netManager.GetPlayerFromIndex(i);
+                        //    netManager.KickPlayerOut(pp);
+                        //});
+                        playerListContainer.transform.GetChild(0).GetChild(1).GetComponent<Text>().text = netManager.participantsList[i]+" (Host)";
+                    }
+                    else
+                    {
+                        playerListContainer.transform.GetChild(0).GetChild(0).GetComponent<Button>().interactable = false;
+                        playerListContainer.transform.GetChild(0).GetChild(1).GetComponent<Text>().text = netManager.participantsList[i] + " (Host)";
+                    }
+                }
+                else
+                {
+                    GameObject btn = Instantiate(playerListContainer.transform.GetChild(0).gameObject, playerListContainer.transform) as GameObject;
+                    
+                    if (netManager.IsHost)
+                    {
+                        btn.transform.GetChild(0).GetComponent<Button>().interactable = true;
+                        btn.transform.GetChild(0).GetComponent<Button>().onClick.RemoveAllListeners();
+                        btn.transform.GetChild(0).GetComponent<Button>().onClick.AddListener(delegate
+                        {
+                            PhotonPlayer pp = netManager.GetPlayerFromIndex(i);
+                            netManager.KickPlayerOut(pp);
+                        });
+                        btn.transform.GetChild(1).GetComponent<Text>().text = netManager.participantsList[i];
+                    }
+                    else
+                    {
+                        btn.transform.GetChild(0).GetComponent<Button>().interactable = false;
+                        btn.transform.GetChild(1).GetComponent<Text>().text = netManager.participantsList[i];
+                    }
+                }
+            }//end for loop
+        }//end populate participants list func
+
+        void PopulateRoomList()
+        {
+            netManager.RefreshRoomList();
+
+            int roomCnt = netManager.RoomCount;
+            if (roomCnt == 0)
+            {
+                roomListContainer.transform.GetChild(0).GetComponent<Button>().interactable = false;
+                roomListContainer.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "NA";
+                return;
+            }
+
+            for (int i = roomListContainer.transform.childCount-1; i >0; i--)
+                Destroy(roomListContainer.transform.GetChild(i));
+
+            for(int i=0;i<roomCnt;i++)
+            {
+                if(i==0)
+                {
+                    int ind = i;
+                    roomListContainer.transform.GetChild(0).GetComponent<Button>().interactable = true;
+                    roomListContainer.transform.GetChild(0).GetComponent<Button>().onClick.RemoveAllListeners();
+                    roomListContainer.transform.GetChild(0).GetComponent<Button>().onClick.AddListener(delegate { JoinRoom(ind); });
+                    roomListContainer.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = netManager.roomList[i]+"("+netManager.roomPlayerCnt[i].ToString()+")";
+
+                }
+                else
+                {
+                    int ind = i;
+                    GameObject btn = Instantiate(roomListContainer.transform.GetChild(0).gameObject,roomListContainer.transform) as GameObject;
+                    btn.GetComponent<Button>().interactable = true;
+                    btn.GetComponent<Button>().onClick.RemoveAllListeners();
+                    btn.GetComponent<Button>().onClick.AddListener(delegate { JoinRoom(ind); });
+                    btn.transform.GetChild(0).GetComponent<Text>().text = netManager.roomList[i] + "(" + netManager.roomPlayerCnt[i].ToString() + ")";
+
+                }
+            }
+        }//end Populate room list func
+    }
+}
---
+++ Random/NetworkManager.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ExitGames.Client.Photon;
+
+namespace Shubham.Networking
+{
+    public class NetworkManager : Photon.PunBehaviour
+    {
+        public delegate void OnConnectedToServer(bool success);
+        public delegate void OnRoomJoined(bool success);
+        public delegate void OnNewPlayerJoined(PhotonPlayer player);
+
+        public static event OnConnectedToServer ConnectedToPhoton;
+        public static event OnRoomJoined RoomJoined;
+        public static event OnNewPlayerJoined NewPlayerJoined;
+
+        
+        /// <summary>
+        /// List populated after connecting to photon server, listing out all the available and public rooms
+        /// </summary>
+        public List<string> roomList = new List<string>();
+        public List<int> roomPlayerCnt = new List<int>();
+
+        public List<string> participantsList = new List<string>();
+
+        #region getter_setter methods
+
+        public bool IsHost
+        {
+            get { return isHost; }
+        }
+
+        public string PlayerName
+        {
+            get { return playerName; }
+            set { playerName = value; }
+        }
+
+        public string RoomName
+        {
+            get { return roomName; }
+        }
+
+        public int RoomCount
+        {
+            get { return roomList.Count; }
+        }
+
+        public int ParticipantsCount
+        {
+            get { return participantsList.Count; }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// To maintain the server versions
+        /// </summary>
+        private string gameVersion = "v1.0";
+        /// <summary>
+        /// The room name used to connect or join game server
+        /// </summary>
+        private string roomName;
+        /// <summary>
+        /// Flag value true is the user is host of current session
+        /// </summary>
+        private bool isHost = false;
+        /// <summary>
+        /// Player name to identify him in room
+        /// </summary>
+        private string playerName = "default";
+
+        private void Awake()
+        {
+            PhotonNetwork.automaticallySyncScene = true;
+
+            if (LobbyController.Instance.netManager == null)
+                LobbyController.Instance.netManager = this;
+        }
+
+        private void Start()
+        {
+            PhotonNetwork.ConnectUsingSettings(gameVersion);
+            if (PlayerPrefs.HasKey("PlayerName"))
+            {
+                PlayerName = PlayerPrefs.GetString("PlayerName");
+            }
+            else
+            {
+                PlayerName = "Test_" + Random.Range(100, 999).ToString();
+                PlayerPrefs.SetString("PlayerName", playerName);
+            }
+        }
+
+        /// <summary>
+        /// Call this to Host room if isHost or else join as attendee
+        /// </summary>
+        /// <param name="roomName"></param>
+        /// <param name="isHost"></param>
+        public void StartOrJoinRoom(string roomName, bool isHost=false)
+        {
+            this.roomName = roomName;
+            this.isHost = isHost;
+            if (isHost)
+            {
+                RoomOptions opts = new RoomOptions();
+                opts.IsVisible = true;
+                opts.MaxPlayers = 10;
+                opts.IsOpen = true;
+                PhotonNetwork.CreateRoom(roomName, opts, TypedLobby.Default);
+            }
+            else
+            {
+                PhotonNetwork.JoinRoom(roomName);
+            }
+}
+
+        public void RefreshRoomList()
+        {
+            RoomInfo[] info = PhotonNetwork.GetRoomList();
+            roomList.Clear();
+            roomPlayerCnt.Clear();
+            foreach(RoomInfo ri in info)
+            {
+                roomList.Add(ri.Name);
+                roomPlayerCnt.Add(ri.PlayerCount);
+            }
+        }
+
+        public void RefreshParticipantsList(string roomName)
+        {
+            PhotonPlayer[] playerInfo = PhotonNetwork.playerList;
+
+            participantsList.Clear();
+
+            foreach (PhotonPlayer pp in playerInfo)
+            {
+                participantsList.Add(pp.NickName);
+            }
+        }
+        
+        public void KickPlayerOut(PhotonPlayer player)
+        {
+            PhotonNetwork.CloseConnection(player);
+        }
+
+        public PhotonPlayer GetPlayerFromIndex(int participantIndex)
+        {
+            PhotonPlayer[] playerInfo = PhotonNetwork.playerList;
+            return playerInfo[participantIndex];
+        }
+
+        #region pun_overrides
+
+        public override void OnConnectedToPhoton()
+        {
+            Debug.Log("Connected to Photon");
+            RefreshRoomList();
+            PhotonNetwork.playerName = this.playerName;
+            if (ConnectedToPhoton != null)
+                ConnectedToPhoton.Invoke(true);
+        }
+
+        public override void OnFailedToConnectToPhoton(DisconnectCause cause)
+        {
+            Debug.Log("Failed to connect to Photon: "+cause.ToString());
+            if (ConnectedToPhoton != null)
+                ConnectedToPhoton.Invoke(false);
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("Joined room successfully");
+            if (RoomJoined != null)
+                RoomJoined.Invoke(true);
+        }
+
+        public override void OnPhotonJoinRoomFailed(object[] codeAndMsg)
+        {
+            Debug.Log("Failed to join room. " + codeAndMsg[1].ToString());
+            if (RoomJoined != null)
+                RoomJoined.Invoke(false);
+        }
+
+        public override void OnDisconnectedFromPhoton()
+        {
+            Debug.Log("Disconnected from Photon");
+        }
+
+        public override void OnPhotonPlayerConnected(PhotonPlayer newPlayer)
+        {
+            Debug.Log("New player joined: " + newPlayer.NickName);
+            if (NewPlayerJoined != null)
+                NewPlayerJoined.Invoke(newPlayer);
+        }
+
+        #endregion
+
+    }//end of Network manager class
+
+}//end of namespace
