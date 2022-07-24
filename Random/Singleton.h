137964b5faaefcc918b37e47ca41768ab3b616cf
Shubham Singh <shubham.nfynt@gmail.com>
2022-07-24 13:02:32 +0530

    Update Singleton.h

--- Random/Singleton.h
+++ Random/Singleton.h
@@ -1,41 +1,33 @@
-#pragma once
+#ifndef NFYNT_SINGLETON_H_
+#define NFYNT_SINGLETON_H_
 
-#define UWC_SINGLETON(ClassName) \
-    private:\
-        ClassName() = default;\
-        ~ClassName() = default;\
-    public:\
-        ClassName(const ClassName&) = delete;\
-        ClassName& operator=(const ClassName&) = delete;\
-        ClassName(ClassName&&) = delete;\
-        ClassName& operator=(ClassName&&) = delete;\
-        \
-        static bool IsNull()\
-        {\
-            return instance == nullptr;\
-        }\
-        \
-        static ClassName& Get()\
-        {\
-            return *instance;\
-        }\
-        \
-        static void Create()\
-        {\
-            if (!instance) instance = new ClassName;\
-        }\
-        \
-        static void Destroy()\
-        {\
-            if (instance)\
-            {\
-                delete instance;\
-                instance = nullptr;\
-            }\
-        }\
-    private:\
-        static ClassName* instance;
+#include <memory>
+#include <mutex>
 
+template <typename T> class Singleton
+{
+public:
+	virtual ~Singleton() = default;
+	static T &Get()
+	{
+		std::call_once( m_onceFlag,
+            []
+			{
+				m_Instance.reset( new T() );
+			} );
+		return *m_Instance.get();
+	}
 
-#define UWC_SINGLETON_INSTANCE(ClassName) \
-    ClassName* ClassName::instance = nullptr;
+protected:
+	static std::unique_ptr<T> m_Instance;
+
+	static std::once_flag m_onceFlag;
+	Singleton() = default;
+    // disable auto-generated move/copy methods
+	Singleton( const Singleton &src ) = delete;
+	Singleton &operator=( const Singleton &rhs ) = delete;
+};
+template<typename T> std::once_flag Singleton<T>::m_onceFlag;
+template<typename T> std::unique_ptr<T> Singleton<T>::m_Instance;
+
+#endif //NFYNT_SINGLETON_H_
